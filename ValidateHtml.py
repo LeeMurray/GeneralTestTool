12f9109dbabbe6e123502edf37b55faf6b42f131
Lee Murray <lmurray@appliedsystems.com>
2019-10-24 02:04:38 +0100

    [IGNORE] - Removal of file

--- ValidateHtml.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-# t = lorem.text(i)
-i = int(input("Enter the string length you require : "))
-
-t = ''.join(random.choice(string.ascii_lowercase) for x in range(i))
-
-print(t)
