9d516f1355d43034fa05304e67ee73848b12a30e
Lee Murray <lmurray@appliedsystems.com>
2019-10-24 14:47:00 +0100

    [IGNORE] - Quick clean up of old code

--- StringGen.py
+++ StringGen.py
@@ -1,15 +1,7 @@
 from tkinter import simpledialog
 from tkinter import messagebox
 import pyperclip
-# import random
-# import string
 import lorem
-
-
-# def string_generator():
-    # i = simpledialog.askinteger(title="Number", prompt="Enter the number of characters you need : ")
-    # t = ''.join(random.choice(string.ascii_lowercase) for x in range(i))
-    # pyperclip.copy(t)
 
 
 def ipsum_string():
@@ -18,10 +10,8 @@ def ipsum_string():
 
     if d > len(c):
         messagebox.showinfo("Error", "Request Too Long. Paragraph is only " + str(len(c)) + " characters long. Please try again.")
-        # print("Request Too Long. Paragraph is only" + str(len(c)) + "long. Please try again.")
     elif d == 0:
         messagebox.showinfo("Error", "Can not preform a request for a 0 string length.")
-        # print("You want a string of 0 whats the point...")
     else:
         count = c[0:d]
         pyperclip.copy(count)
