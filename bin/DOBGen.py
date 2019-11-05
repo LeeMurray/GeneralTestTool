d538b8f734d7b64d06fb6fc3710380dc3c5b8f56
Lee Murray <lmurray@appliedsystems.com>
2019-11-05 20:28:30 +0000

    Improvements and Furture proofing DOB Gen

--- bin/DOBGen.py
+++ bin/DOBGen.py
@@ -1,12 +1,19 @@
 import random
 import pyperclip
+import datetime
 from bin import Message
+from dateutil.relativedelta import relativedelta
+import datetime
 
 
 def generate_date():
+    x = (datetime.datetime.now())
+    end_year = x.year - 18
+    start_year = x.year - 80
+
     d = str(random.randrange(1, 28)).zfill(2)
     m = str(random.randrange(1, 12)).zfill(2)
-    y = str(random.randrange(1939, 2001))
+    y = str(random.randrange(start_year, end_year))
     string = str(d + "/" + m + "/" + y)
 
     pyperclip.copy(string)
